00caacd56dae887be8e6b03d6761d3e1ace75b2f
Niv Sardi <xaiki@evilgiggle.com>
2016-01-22 14:16:16 -0300

    add formated date and split out case of no selected episode
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import moment from 'moment';
+
 import ButterList from './List';
 import style from './styl/show_detail.styl';
 
@@ -45,17 +47,23 @@ let EpisodeList = (props) => (
               items={props.episodes} itemComponent={EpisodeItem}/>
 );
 
+let EpisodeInfoReal = (props) => (
+    <div className={style['episode-info']}>
+        <div className={style['episode-info-title']}>{props.title}</div>
+        <div className={style['episode-info-number']}>{props.episode}</div>
+        <div data-toggle="tooltip" data-placement="left"
+             title={i18n.__('Health Unknown')} className="fa fa-circle health-icon None"></div>
+        <div data-toggle="tooltip" data-placement="left" title={i18n.__('Magnet link')} className="fa fa-magnet show-magnet-link"></div>
+        <div className={style['episode-info-date']}>
+            {moment(props['first_aired']).format('LLLL')}
+        </div>
+        <div className={style['episode-info-description']}>{props.overview}</div>
+    </div>
+)
+
 let EpisodeInfo = (props) => (
     <div className={style['episode-info-container']}>
-        <div className={style['episode-info']}>
-            <div className={style['episode-info-title']}>{props.title}</div>
-            <div className={style['episode-info-number']}>{props.episode}</div>
-            <div data-toggle="tooltip" data-placement="left"
-                 title={i18n.__('Health Unknown')} className="fa fa-circle health-icon None"></div>
-            <div data-toggle="tooltip" data-placement="left" title={i18n.__('Magnet link')} className="fa fa-magnet show-magnet-link"></div>
-            <div className={style['episode-info-date']}>{props['first_aired']}</div>
-            <div className={style['episode-info-description']}>{props.overview}</div>
-        </div>
+        {props.title?<EpisodeInfoReal {...props} />:<p>no selected episode</p>}
     </div>
 )
 
